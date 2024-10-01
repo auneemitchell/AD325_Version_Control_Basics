ed6e198435e0e034439392a0fe19cb8e762656b2
Aune Mitchell <auneemitchell@gmail.com>
2024-09-30 19:30:42 -0700

    Adding complete_task feature. Also adding check that index is valid for delete_task and docstrings for all modules, class, and functions.

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
+"""
+app.py module runs the to-do list application.
+"""
+
 import argparse
 from task_manager import TaskManager
 
 def main():
+    """Main function creates arguments for application and ties them to task_manager functions."""
     parser = argparse.ArgumentParser(description='To-Do List Application')
     parser.add_argument('--add', metavar='TASK', type=str, help='Add a new task')
     parser.add_argument('--view', action='store_true', help='View all tasks')
     parser.add_argument('--delete', metavar='INDEX', type=int, help='Delete a task by its index')
+    parser.add_argument('--complete', metavar='INDEX', type=int, help='Mark a task as completed')
     args = parser.parse_args()
 
     manager = TaskManager()
@@ -16,6 +22,8 @@ def main():
         manager.view_tasks()
     elif args.delete is not None:
         manager.delete_task(args.delete - 1)  # Adjust for zero-based index
+    elif args.complete is not None:
+        manager.complete_task(args.complete - 1)  # Adjust for zero-based index
     else:
         parser.print_help()
 
